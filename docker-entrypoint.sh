9cee21be49bbeffff2debad168414a2e9df5bb9e
AshotIkilikyan12 <ikilikyan.2012@gmail.com>
2026-07-18 15:01:58 +0400

    Fix: Move entrypoint logic directly into Dockerfile CMD

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Աշխատեցնում ենք միգրացիաները միայն միացման պահին
-php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
-
-# Գործարկում ենք Apache-ն հիմնական ռեժիմով
-exec apache2-foreground
